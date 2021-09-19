20473817e2159545676893125dc8a0e7600433a3
den1ohar <sg.d.ohar@gmail.com>
2021-09-19 15:51:42 +0300

    asdasd

--- client/src/components/ContentBlock.tsx
+++ client/src/components/ContentBlock.tsx
@@ -17,7 +17,7 @@ const ContentBlock: React.FC<ContentInterfaceProps> = ({
   textTitle = "Text Title",
   textParagraph = "Text Paragraph",
   headTitle = "Head Title",
-  headDesc = "Head Descriptions Descriptions"
+  headDesc = "Head  Descriptions"
 }) => {
   const classes = useStyles();
   return (
